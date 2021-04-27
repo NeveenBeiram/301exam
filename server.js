5b87a049c7319c6554386e3089b5acd8a1fbf71a
NeveenBeiram <neveenbeiram@yahoo.com>
2021-04-27 13:40:59 +0300

    not done

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ server.use(methodOverride('_method'));
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
-  // ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false }
 });
 
 
